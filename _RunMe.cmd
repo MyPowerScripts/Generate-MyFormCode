8f69eac070afd62d6561661c5603f47420ec197f
My Power Scripts <34432291+MyPowerScripts@users.noreply.github.com>
2022-05-06 22:39:10 -0700

    Update

--- _RunMe.cmd
+++ _RunMe.cmd
@@ -1,9 +1,9 @@
 @Echo off
 
 ::Set MyApp=Generate_MyFormCode_5.0.0.4
-Set MyApp=Generate_MyFormCode_6.0.0.4
+Set MyApp=Generate_MyFormCode_6.0.9.9
 
-::"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy ByPass -File "%~dp0%MyApp%.ps1"
-"C:\Program Files\PowerShell\7\pwsh.exe" -NoProfile -ExecutionPolicy ByPass -File "%~dp0%MyApp%.ps1"
+"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy ByPass -File "%~dp0%MyApp%.ps1"
+::"C:\Program Files\PowerShell\7\pwsh.exe" -NoProfile -ExecutionPolicy ByPass -File "%~dp0%MyApp%.ps1"
 
 Pause
