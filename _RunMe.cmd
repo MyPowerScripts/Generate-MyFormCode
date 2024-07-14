058816ad014135bf5619baa1b2500a5b9b7a7a4a
My Power Scripts <ken.sweet@MyPowerScripts.com>
2024-07-13 19:28:58 -0700

    Update

--- _RunMe.cmd
+++ _RunMe.cmd
@@ -1,6 +1,6 @@
 @Echo off
 
-Set MyApp=Generate_MyFormCode_6.1.1.3
+Set MyApp=Generate_MyFormCode
 
 "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy ByPass -File "%~dp0%MyApp%.ps1"
 
