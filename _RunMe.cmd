c48cc825c7fcabfd9627e3dae6f152b96c2dea18
My Power Scripts <ken.sweet@MyPowerScripts.com>
2025-08-26 11:45:25 -0700

    Update to Version 7.x

--- _RunMe.cmd
+++
@@ -1,7 +0,0 @@
-@Echo off
-
-Set MyApp=Generate_MyFormCode
-
-"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy ByPass -File "%~dp0%MyApp%.ps1"
-
-::Pause
--- _RunMeNew.cmd
+++
@@ -1,9 +0,0 @@
-@Echo off
-
-Set MyApp=Generate_MyNewFormCode
-
-"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy ByPass -File "%~dp0%MyApp%.ps1"
-
-::Pause
-
-
